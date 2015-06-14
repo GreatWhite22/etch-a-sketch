54d245ba0d22f5c2ab560b8d12a3e52b8fd6dd31
Connor Lewis <connorlewis@utexas.edu>
2015-06-13 23:15:12 -0500

    16x16 grid working

--- sketch.js
+++ sketch.js
@@ -3,13 +3,18 @@ $(document).ready(function(){
 	var width = 16;
 	$("#grid").append("<table class='board'>test</table>");
 	for (var i = 16; i > 0; i--) {
-		$(".board").append("<tr><td>hello</td><td>hi</td></tr>");
-		$('td').css("height", "35px");
-		$('td').css("width", "35px");
-		$('td').css("background-color", "gray");
-		$('td').css({"border-color": "black", 
-             "border-width":"2px", 
-             "border-style":"solid"});
+		$(".board").append("<tr><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/></tr>");
 		size --;
 	}
+	$('td').css("height", "35px");
+	$('td').css("width", "35px");
+	$('td').css("background-color", "white");
+	$('td').css({"border-color": "black", 
+		"border-width":"2px", 
+		"border-style":"solid"});
+	$("td").hover(function(){
+		$(this).css("background-color", "blue");
+	},function(){}
+	);
+
 });
