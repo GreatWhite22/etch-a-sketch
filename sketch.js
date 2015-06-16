dcb8715928c9e87fad28525e7a20fc7f7cfe1926
Connor Lewis <connorlewis@utexas.edu>
2015-06-15 22:42:48 -0500

    Finished

--- sketch.js
+++ sketch.js
@@ -1,20 +1,40 @@
+var height = 16;
+var width = 16;
+var boxSize = 35
 $(document).ready(function(){
-	var size = 16;
-	var width = 16;
-	$("#grid").append("<table class='board'>test</table>");
-	for (var i = 16; i > 0; i--) {
-		$(".board").append("<tr><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/><td/></tr>");
-		size --;
+	newgrid();
+	mouseHover();
+	$(":button").click(function(){
+	height = prompt("Enter a new grid size");
+	width = height;
+	boxSize = 560/height;
+	$(".board").replaceWith(newgrid());
+	mouseHover();
+});
+
+});
+
+function newgrid(){
+	$("#grid").append("<table class='board'></table>");
+	for (var i = 0; i < height; i++){
+		$(".board").append("<tr></tr>");
 	}
-	$('td').css("height", "35px");
-	$('td').css("width", "35px");
+	for (var i = 0; i < width; i++){
+		$("tr").append("<td></td>");
+	}
+	$('td').css("height", boxSize);
+	$('td').css("width", boxSize);
 	$('td').css("background-color", "white");
 	$('td').css({"border-color": "black", 
 		"border-width":"2px", 
 		"border-style":"solid"});
+	return;
+};
+
+function mouseHover(){
 	$("td").hover(function(){
 		$(this).css("background-color", "blue");
 	},function(){}
 	);
-
-});
+	return;
+}
